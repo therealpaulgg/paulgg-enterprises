50e6fff2c20c88ec6e7411ccd0965134f4f8c011
therealpaulgg <paul@paul.systems>
2021-09-06 01:50:12 -0400

    fix typescript bugs

--- src/components/inputs/Button.vue
+++ src/components/inputs/Button.vue
@@ -2,23 +2,21 @@
     <button
         :type="type"
         :disabled="disabled"
-        :class="
-            `${
-                active
-                    ? 'bg-custom-light-pink text-black'
-                    : 'bg-custom-titanium dark:bg-gray-300'
-            } text-white dark:text-black shadow text-sm transition duration-200 m-1 btn ${
-                uppercase ? 'uppercase' : ''
-            } py-2 px-4 ${
-                circular
-                    ? 'w-12 h-12 rounded-full flex items-center justify-center'
-                    : 'rounded '
-            } ${
-                disabled
-                    ? 'opacity-50 cursor-not-allowed '
-                    : 'hover:bg-custom-light-pink dark:hover:text-white dark:hover:bg-custom-light-pink hover:text-white '
-            }`
-        "
+        :class="`${
+            active
+                ? 'bg-custom-light-pink text-black'
+                : 'bg-custom-titanium dark:bg-gray-300'
+        } text-white dark:text-black shadow text-sm transition duration-200 m-1 btn ${
+            uppercase ? 'uppercase' : ''
+        } py-2 px-4 ${
+            circular
+                ? 'w-12 h-12 rounded-full flex items-center justify-center'
+                : 'rounded '
+        } ${
+            disabled
+                ? 'opacity-50 cursor-not-allowed '
+                : 'hover:bg-custom-light-pink dark:hover:text-white dark:hover:bg-custom-light-pink hover:text-white '
+        }`"
     >
         <slot />
     </button>
@@ -36,7 +34,7 @@ export default defineComponent({
             default: true,
         },
         type: {
-            type: String,
+            type: String as () => "button" | "reset" | "submit",
             default: "button",
         },
     },
